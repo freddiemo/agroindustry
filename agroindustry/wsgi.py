65d0203d90f8e770e7ee62b5d5cffc26e93b781a
Freddie Molina <freddiejmo@gmail.com>
2020-03-07 20:44:55 -0400

    rename settings

--- agroindustry/wsgi.py
+++ agroindustry/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for agroindustria project.
+WSGI config for agroindustry project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agroindustria.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agroindustry.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agroindustria.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agroindustry.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
